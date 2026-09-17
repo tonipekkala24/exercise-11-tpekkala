fe069d0d8ea8718f324cda7df8ac3ad2ab31f1e1
tonipekkala24 <tpekkala24@students.oamk.fi>
2026-09-17 13:45:46 +0300

    questions added

--- js/functions.js
+++ js/functions.js
@@ -1,8 +1,38 @@
 const questions = [
-    "Mikä Azure-palvelu tarjoaa palveluttoman (serverless) koodin suoritusympäristön?",
-    "Mikä on edullisin Azure Blob -tallennusluokka pitkäaikaiselle arkistodatalle?",
-    "Millä palvelulla luodaan suora ja suojattu yksityisyhteys yrityksen toimitiloista Azureen ilman julkista internetiä?",
-    "Mikä Azuren työkalu jakaa verkkoliikennettä (Layer 7) ja sisältää palomuurin web-sovelluksille?",
-    "Mitä nimitystä käytetään Azuren alueen sisäisistä, fyysisesti erillistä konesaleista, jotka takaavat korkean käytettävyyden?"
-]
+    {
+        question: "Mikä Azure-palvelu tarjoaa palveluttoman (serverless) koodin suoritusympäristön?",
+        
+    },
+    {
+        question: "Mikä on edullisin Azure Blob -tallennusluokka pitkäaikaiselle arkistodatalle?",
+        
+    },
+    {
+        question: "Millä palvelulla luodaan suora ja suojattu yksityisyhteys yrityksen toimitiloista Azureen ilman julkista internetiä?",
+        
+    },
+    {
+        question: "Mikä Azuren työkalu jakaa verkkoliikennettä (Layer 7) ja sisältää palomuurin web-sovelluksille?",
+        
+    },
+    {
+        question: "Mitä nimitystä käytetään Azuren alueen sisäisistä, fyysisesti erillisistä konesaleista, jotka takaavat korkean käytettävyyden?",
+        
+    }
+];
 
+const quiz = document.getElementById("quiz");
+const button = document.getElementById("showAnswers");
+
+questions.forEach((item, index) => {
+    const questionElement = document.createElement("div");
+
+    questionElement.innerHTML = `
+    <h2>${index + 1}. ${item.question}</h2>
+        <p class="answer" style="display: none;">
+            Vastaus: ${item.answer}
+        </p>
+    `;
+
+    quiz.appendChild(questionElement);
+});

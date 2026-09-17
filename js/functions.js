4ac7dbf60790b1276f7488664abc1cb0658b3698
tonipekkala24 <tpekkala24@students.oamk.fi>
2026-09-17 13:03:27 +0300

    Initial commit

---
+++ js/functions.js
@@ -0,0 +1,8 @@
+const questions = [
+    "Mikä Azure-palvelu tarjoaa palveluttoman (serverless) koodin suoritusympäristön?",
+    "Mikä on edullisin Azure Blob -tallennusluokka pitkäaikaiselle arkistodatalle?",
+    "Millä palvelulla luodaan suora ja suojattu yksityisyhteys yrityksen toimitiloista Azureen ilman julkista internetiä?",
+    "Mikä Azuren työkalu jakaa verkkoliikennettä (Layer 7) ja sisältää palomuurin web-sovelluksille?",
+    "Mitä nimitystä käytetään Azuren alueen sisäisistä, fyysisesti erillistä konesaleista, jotka takaavat korkean käytettävyyden?"
+]
+

14a5390f9d6ff32c15ed45dd288afa86d58fcd08
tonipekkala24 <tpekkala24@students.oamk.fi>
2026-09-17 13:46:50 +0300

    answers added

--- js/functions.js
+++ js/functions.js
@@ -1,23 +1,23 @@
 const questions = [
     {
         question: "Mikä Azure-palvelu tarjoaa palveluttoman (serverless) koodin suoritusympäristön?",
-        
+        answer: "Azure Functions"
     },
     {
         question: "Mikä on edullisin Azure Blob -tallennusluokka pitkäaikaiselle arkistodatalle?",
-        
+        answer: "Archive"
     },
     {
         question: "Millä palvelulla luodaan suora ja suojattu yksityisyhteys yrityksen toimitiloista Azureen ilman julkista internetiä?",
-        
+        answer: "Azure ExpressRoute"
     },
     {
         question: "Mikä Azuren työkalu jakaa verkkoliikennettä (Layer 7) ja sisältää palomuurin web-sovelluksille?",
-        
+        answer: "Azure Application Gateway (WAF)"
     },
     {
         question: "Mitä nimitystä käytetään Azuren alueen sisäisistä, fyysisesti erillisistä konesaleista, jotka takaavat korkean käytettävyyden?",
-        
+        answer: "Availability Zones"
     }
 ];
 
@@ -36,3 +36,11 @@ questions.forEach((item, index) => {
 
     quiz.appendChild(questionElement);
 });
+
+button.addEventListener("click", () => {
+    const answers = document.querySelectorAll(".answer");
+
+    answers.forEach(answer => {
+        answer.style.display = "block"
+    })
+})
